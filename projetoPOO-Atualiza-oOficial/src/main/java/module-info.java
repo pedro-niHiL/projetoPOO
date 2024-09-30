d995eb5823894916be2b965129ce6a91113be912
JonasBrothers12 <168020773+JonasBrothers12@users.noreply.github.com>
2024-09-30 19:01:06 -0300

    Delete projetoPOO-Atualiza-oOficial directory

--- projetoPOO-Atualiza-oOficial/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.projetopoo {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens com.example.projetopoo to javafx.fxml;
-    exports com.example.projetopoo;
-}
